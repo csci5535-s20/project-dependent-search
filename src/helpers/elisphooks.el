e72c6fb71d3f61a81bdc3bc86b85f822b5d48403
Michael Dresser <michaelmdresser@gmail.com>
2020-05-05 21:54:15 -0600

    edited paths and commented optional hook

--- src/helpers/elisphooks.el
+++ src/helpers/elisphooks.el
@@ -1,18 +1,18 @@
   (defun agda-index-hook ()
     (when (equal major-mode 'agda2-mode)
-      (shell-command (concat "/home/delta/devel/csci5535/project/parser " buffer-file-name)))
+      (shell-command (concat "/path/to/parser " buffer-file-name)))
     )
-  (add-hook 'after-save-hook 'agda-index-hook)
+  ; (add-hook 'after-save-hook 'agda-index-hook) ; optional on-save hook to always build an index, not necessary
 
   (defun agda-type-search ()
     (interactive)
     (when (equal major-mode 'agda2-mode)
       ; first rebuild the index, since we have a new query there is no reason to keep an old one
-      (shell-command (concat "/home/delta/devel/csci5535/project/parser " buffer-file-name))
+      (shell-command (concat "/path/to/parser " buffer-file-name))
       ; then add the query to the index
       (shell-command
        (concat
-        "/home/delta/devel/csci5535/project/querybuilder "
+        "/path/to/querybuilder "
         buffer-file-name
         " '"
         (read-string "Enter type signature:")
